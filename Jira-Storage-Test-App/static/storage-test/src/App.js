be28bb89e818c8ac7b721d6811dffb269bf47525
ABHISHEK SRIVASTAVA <30977985+ABHISHEK611@users.noreply.github.com>
2022-07-07 11:28:07 +0530

    Update App.js
    extended code funs

--- Jira-Storage-Test-App/static/storage-test/src/App.js
+++ Jira-Storage-Test-App/static/storage-test/src/App.js
@@ -2,11 +2,18 @@ import React, { useEffect, useState, Fragment } from 'react';
 import { invoke, router } from '@forge/bridge';
 
 function App() {
+	const [data, setData] = useState(null);
     const [data1, setData1] = useState(null);
 
+	 useEffect(() => {
+        invoke('getProjectStory', { example: 'my-invoke-variable' }).then(setData);
+    }, []);
+	
+	
     useEffect(() => {
         invoke('getStoryPoint', { example: 'my-invoke-variable' }).then(setData1);
     }, []);
+	
 
     return (
 	<>
@@ -15,6 +22,22 @@ function App() {
 			<p>Current username: {basicDetails.name} <br></br> Current user email-id: {basicDetails.email}</p>
 			))
 			: 'Loading...'}
+        </div>
+		
+		<div>
+			<table>
+				<tr>
+					<th>Issue Key</th>
+					<th>Story Point</th>
+				</tr>
+			{data ? data.map(issue =>(
+				<tr>
+					<td>{issue.key}</td>
+					<td>{issue.sp}</td>
+				</tr>
+			))
+			: 'Loading...'}
+			</table>
         </div>
 	</>
     );
