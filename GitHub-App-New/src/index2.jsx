82ffc7e95fc5006469a04155638262ac523598e0
ABHISHEK611 <abhi.scorpio611@gmail.com>
2022-07-28 17:22:27 +0530

    github oauth

--- GitHub-App-New/src/index2.jsx
+++ GitHub-App-New/src/index2.jsx
@@ -3,30 +3,31 @@ import { fetch } from '@forge/api';
 
 const App2 = () =>
 {
-
-    const [avatarUrl, setAvatarUrl] = useState(undefined);
-
-    useEffect(() => {
-        fetch("https://api.github.com/users/ABHISHEK611")
-        .then((res) => res.json())
-        .then(
-            (result) => {
-                console.log(result);
-                setAvatarUrl(result.avatar_url);
-            },
-            (error) => {
-                console.log(error);
-            }
-            );
-    },[]);
+    console.log("inside app2");
+    const [data] = useState(async () => {
+        const github = api.asUser().withProvider('github', 'github-apis')
+        if (!await github.hasCredentials()) {
+          await github.requestCredentials()
+        }
+        const response = await github.fetch('/user');
+        if (response.status === 401)  {
+          await github.requestCredentials();
+        }
+        if (response.ok) {
+          return response.json()
+        }
+        
+        return {
+          status: response.status,
+          statusText: response.statusText,
+          text: await response.text(),
+        }
+      })
+      console.log("run index");
 
     return (
         <Fragment>
-          <Text>Welcome to GitHub Integration</Text>
-          <Image
-            src={avatarUrl}
-            alt="ImageOfGitHubAccount"
-          />
+          <Text>Welcome {data.login}</Text>
         </Fragment>
       );
 };
