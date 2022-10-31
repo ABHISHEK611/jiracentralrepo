fae1c0d988995cfd5709e53f8dd52ab5e36a1aae
ABHISHE611 <abhi.scorpio611@gmail.com>
2022-10-31 10:05:45 +0530

    react v2.0

--- JiraTreePlugin/static/hello-world/src/App.js
+++ JiraTreePlugin/static/hello-world/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+/*import React, { useEffect, useState } from 'react';
 import { invoke } from '@forge/bridge';
 import TableTree from '@atlaskit/table-tree';
 
@@ -53,4 +53,68 @@ export default () => (
   />
 );
 
-//export default App;
+//export default App;*/
+import React, { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import { TreeDataState, CustomTreeData} from '@devexpress/dx-react-grid';
+import { Grid, Table, TableHeaderRow, TableTreeColumn} from '@devexpress/dx-react-grid-material-ui';
+import {  generateRows, defaultColumnValues } from './demo-data/generator';
+
+const getChildRows = (row, rootRows) => (row ? row.items : rootRows);
+
+export default () => {
+  const [columns] = useState([
+    { name: 'name', title: 'Name' },
+    { name: 'gender', title: 'Gender' },
+    { name: 'city', title: 'City' },
+    { name: 'car', title: 'Car' },
+  ]);
+  const [data] = useState(generateRows({
+    columnValues: {
+      ...defaultColumnValues,
+      items: ({ random }) => (random() > 0.5
+        ? generateRows({
+          columnValues: {
+            ...defaultColumnValues,
+            items: () => (random() > 0.5
+              ? generateRows({
+                columnValues: {
+                  ...defaultColumnValues,
+                },
+                length: Math.trunc(random() * 5) + 1,
+                random,
+              })
+              : null),
+          },
+          length: Math.trunc(random() * 3) + 1,
+          random,
+        })
+        : null),
+    },
+    length: 3,
+  }));
+  const [tableColumnExtensions] = useState([
+    { columnName: 'name', width: 300 },
+  ]);
+
+  return (
+    <Paper>
+      <Grid
+        rows={data}
+        columns={columns}
+      >
+        <TreeDataState />
+        <CustomTreeData
+          getChildRows={getChildRows}
+        />
+        <Table
+          columnExtensions={tableColumnExtensions}
+        />
+        <TableHeaderRow />
+        <TableTreeColumn
+          for="name"
+        />
+      </Grid>
+    </Paper>
+  );
+};
