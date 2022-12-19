7dcc1f7e9ed8e7c47a8b3855755d88c0c125c78c
ABHISHEK611 <abhi.scorpio611@gmail.com>
2022-12-19 18:19:54 +0530

    react beaty dnd library try puts

---
+++ myreactapp/src/data/data.js
@@ -0,0 +1,61 @@
+export const data = [
+    {
+      key: "KEY-1",
+      parent: "root",
+      type: "Feature",
+      summary: "Summary of Feature",
+      storypoint: 8,
+      assignee: "Raj",
+      status: "In Progress",
+      children: [
+        {
+          key: "KEY-4",
+          parent: "KEY-1",
+          type: "Story",
+          summary: "Summary of Story",
+          storypoint: 5,
+          assignee: "Raj",
+          status: "In Progress",
+          children: [
+            {
+              key: "KEY-6",
+              parent: "KEY-4",
+              type: "Task",
+              summary: "Summary of Task",
+              storypoint: 5,
+              assignee: "Raj",
+              status: "In Progress"
+            }
+          ]
+        },
+        {
+          key: "KEY-5",
+          parent: "KEY-1",
+          type: "Story",
+          summary: "Summary of Story",
+          storypoint: 3,
+          assignee: "Raj",
+          status: "Open"
+        }
+      ]
+    },
+    {
+      key: "KEY-2",
+      parent: "root",
+      type: "Feature",
+      summary: "Summary of Feature",
+      storypoint: 3,
+      assignee: "Thanh",
+      status: "Open"
+    },
+    {
+      key: "KEY-3",
+      parent: "root",
+      type: "Feature",
+      summary: "Summary of Feature",
+      storypoint: 5,
+      assignee: "Thanh",
+      status: "Closed"
+    }
+  ];
+  
