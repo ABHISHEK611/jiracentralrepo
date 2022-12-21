f9e9dff64e35306ddc6552b862d1222ec00ab229
ABHISHEK611 <abhi.scorpio611@gmail.com>
2022-12-21 17:04:28 +0530

    changes reactdnd v1.1

--- JiraReactDnd/static/hello-world/src/index.js
+++ JiraReactDnd/static/hello-world/src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 //import { createRoot } from "react-dom/client";
 
-import App from "./app";
+import App from "./App";
 
 ReactDOM.render(
   <StrictMode>
