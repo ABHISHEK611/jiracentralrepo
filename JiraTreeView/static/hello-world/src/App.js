a9a9a018b7528d1a59dd2e0a359f4eb3dc89dff4
ABHISHEK611 <abhi.scorpio611@gmail.com>
2023-01-13 01:15:37 +0530

    code changes local v1.5

--- JiraTreeView/static/hello-world/src/App.js
+++ JiraTreeView/static/hello-world/src/App.js
@@ -1,11 +1,246 @@
 import React, { useEffect, useRef, useState }  from 'react';
-import { TreeList, Column, RowDragging, ColumnChooser, Editing, RequiredRule, Lookup, Button, } 
-from 'devextreme-react/tree-list';
-import api, { fetch, route } from '@forge/api';
-import ForgeUI, { useProductContext } from '@forge/ui';
-import { requestJira } from '@forge/bridge';
-import { fetchIssueList } from './data';
+// import api, { fetch, route } from '@forge/api';
+// import ForgeUI, { useProductContext } from '@forge/ui';
+// import { requestJira } from '@forge/bridge';
+import {issues} from './data';
 //import {issues as issueList} from './data';
+import {
+  TreeList,
+  ColumnChooser,
+  ColumnFixing,
+  Column,
+  RequiredRule,
+  FilterRow,
+  SearchPanel,
+  Selection,
+  Editing,
+  RowDragging, 
+  Paging
+} from 'devextreme-react/tree-list';
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.onReorder = this.onReorder.bind(this);
+//     this.onAllowDropInsideItemChanged = this.onAllowDropInsideItemChanged.bind(this);
+//     this.onAllowReorderingChanged = this.onAllowReorderingChanged.bind(this);
+//     this.onShowDragIconsChanged = this.onShowDragIconsChanged.bind(this);
+
+//     this.state = {
+//       issues:[],
+//       //issues: issueList,
+//       allowDropInsideItem: true,
+//       allowReordering: true,
+//       showDragIcons: false,
+//       mode: 'select',
+//       allowSearch: true,
+//     };
+//   }
+
+//   async componentDidMount()
+//   {
+//     let a=0;
+//     let issues1 = [];
+//     let x = await fetchIssueList();
+//     console.log("1 inside componentDidMount: ",x);
+//     let y = x.issues.map((element) => {
+//       console.log("1.5 inside componentDidMount: ",element);
+//       debugger;
+//           let item = {
+//                 ID: element.id,
+//                 Head_ID: (element.fields.issuelinks.length != 0
+//                 ? element.fields.issuelinks[0].hasOwnProperty("outwardIssue")
+//                 ? element.fields.issuelinks[0].outwardIssue.id : -1 : -1),
+//                 Issue_Key: element.key,
+//                 Issue_Type: element.fields.issuetype.name,
+//                 ParentIssue: (element.fields.issuelinks.length != 0
+//                   ? element.fields.issuelinks[0].hasOwnProperty("outwardIssue")
+//                   ? element.fields.issuelinks[0].outwardIssue.key : "" : ""),
+//                 Summary: element.fields.summary,
+//                 Assignee: (element.fields.assignee === null? "Unassigned":element.fields.assignee.displayName),
+//                 Reporter: element.fields.reporter.displayName,
+//                 Priority: element.fields.priority.name,
+//             }
+//           console.log("2 inside componentDidMount: ",JSON.stringify(item));
+//         console.log("3 inside componentDidMount: ",JSON.stringify(issues1));
+//         return item;
+//       });
+//       console.log("4 inside componentDidMount new test: ",y);
+//         this.setState({
+//           issues:y,
+//         });
+//   }
+
+//   render() {
+//     const { mode, allowSearch } = this.state;
+    
+//     return (
+//       <div>
+//         <TreeList
+//           id="issues"
+//           dataSource={this.state.issues}
+//           rootValue={-1}
+//           showRowLines={true}
+//           showBorders={true}
+//           defaultExpandedRowKeys={expandedRowKeys}
+//           columnAutoWidth={true}
+//           keyExpr="ID"
+//           parentIdExpr="Head_ID"
+//           onInitNewRow={this.onInitNewRow}
+//           onRowInserted={this.addRow}
+//         >
+
+//         <RowDragging
+//             onDragChange={this.onDragChange}
+//             onReorder={this.onReorder}
+//             allowDropInsideItem={this.state.allowDropInsideItem}
+//             allowReordering={this.state.allowReordering}
+//             showDragIcons={this.state.showDragIcons}
+//         />
+
+//         <Editing
+//             allowUpdating={true}
+//             allowDeleting={true}
+//             allowAdding={true}
+//             mode="pop" 
+//         />
+
+//         <Column allowHiding={false} dataField="Issue_Key"> </Column>
+//         <Column allowHiding={false} dataField="Issue_Type"> <RequiredRule /> <Lookup dataSource={issuestype} /> </Column>
+//         <Column allowHiding={false} dataField="Summary"> <RequiredRule /> </Column>
+//         <Column dataField="Assignee"> <RequiredRule /> </Column>
+//         <Column dataField="Priority"> <RequiredRule /> </Column>
+//         <Column allowHiding={false} type="buttons" caption="Actions">
+//                 <Button name="add" />
+//                 <Button name="edit" />
+//                 <Button name="delete" />
+//                 <Button name="save" onClick={this.addNode} />
+//             </Column>
+//         <ColumnChooser enabled={true} allowSearch={allowSearch} mode={mode} />
+//         </TreeList>
+//       </div>
+//     );
+//   }
+
+//   addNode = async (e) => {
+//     addRow(e);
+//   }
+  
+//   addRow = async (e) => {
+//     console.log("1 inside addRow: ",e.data.Summary);
+//     console.log("1.5 inside addRow: ",e.data.Issue_Type);
+//     let body = {
+//       fields: {
+//         summary: e.data.Summary,
+//         project: {
+//           key: "OEM",
+//         },
+//         issuetype: {
+//           name: e.data.Issue_Type,
+//         },
+//         assignee: {
+//           name: "Abhishek Srivastava",
+//         },
+//         "customfield_10042": "https://google.com",
+//         "customfield_10034": 8
+//       }
+//     };
+
+//     let body1 = JSON.stringify(body);
+//     console.log("2 inside addRow: ",JSON.stringify(body));
+//     const response = await requestJira('/rest/api/3/issue', {
+//       method: 'POST',
+//       headers: {
+//           'Accept': 'application/json',
+//           'Content-Type': 'application/json'
+//       },
+//       body: body1
+//     })
+//   console.log(`Response: ${response.status} ${response.statusText}`);
+//   }
+
+//   onInitNewRow(e) {
+//     console.log("1 onInitNewRow: ",e);
+//     e.data.ID=i;
+//     e.data.Head_ID = -1;
+//     i++;
+//   }
+
+//   onDragChange(e) {
+//     console.log("1 onDragChange: ",e);
+//     const visibleRows = e.component.getVisibleRows();
+//     const sourceNode = e.component.getNodeByKey(e.itemData.ID);
+//     let targetNode = visibleRows[e.toIndex].node;
+
+//     while (targetNode && targetNode.data) {
+//       if (targetNode.data.ID === sourceNode.data.ID) {
+//         e.cancel = true;
+//         break;
+//       }
+//       targetNode = targetNode.parent;
+//     }
+//   }
+
+//   onReorder(e) {
+//     console.log("1 onReorder: ",e);
+//     const visibleRows = e.component.getVisibleRows();
+//     let sourceData = e.itemData;
+//     let { issues } = this.state;
+//     const sourceIndex = issues.indexOf(sourceData);
+
+//     if (e.dropInsideItem) {
+//       sourceData = { ...sourceData, Head_ID: visibleRows[e.toIndex].key };
+//       issues = [
+//         ...issues.slice(0, sourceIndex),
+//         sourceData,
+//         ...issues.slice(sourceIndex + 1),
+//       ];
+//     } else {
+//       const toIndex = e.fromIndex > e.toIndex ? e.toIndex - 1 : e.toIndex;
+//       let targetData = toIndex >= 0 ? visibleRows[toIndex].node.data : null;
+
+//       if (targetData && e.component.isRowExpanded(targetData.ID)) {
+//         sourceData = { ...sourceData, Head_ID: targetData.ID };
+//         targetData = null;
+//       } else {
+//         const headId = targetData ? targetData.Head_ID : -1;
+//         if (sourceData.Head_ID !== headId) {
+//           sourceData = { ...sourceData, Head_ID: headId };
+//         }
+//       }
+
+//       issues = [...issues.slice(0, sourceIndex), ...issues.slice(sourceIndex + 1)];
+
+//       const targetIndex = issues.indexOf(targetData) + 1;
+//       issues = [...issues.slice(0, targetIndex), sourceData, ...issues.slice(targetIndex)];
+//     }
+
+//     this.setState({
+//       issues,
+//     });
+//   }
+
+//   onAllowDropInsideItemChanged(args) {
+//     this.setState({
+//       allowDropInsideItem: args.value,
+//     });
+//   }
+
+//   onAllowReorderingChanged(args) {
+//     this.setState({
+//       allowReordering: args.value,
+//     });
+//   }
+
+//   onShowDragIconsChanged(args) {
+//     this.setState({
+//       showDragIcons: args.value,
+//     });
+//   }
+// }
+
+// export default App;
 
 const expandedRowKeys = [1];
 
@@ -17,162 +252,13 @@ const issuestype = [
   'Bug Fix Steps',
 ];
 
-const addRow = (e) => {
-  console.log("inside addRow",e);
-};
-let i=0;
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.onReorder = this.onReorder.bind(this);
-    this.onAllowDropInsideItemChanged = this.onAllowDropInsideItemChanged.bind(this);
-    this.onAllowReorderingChanged = this.onAllowReorderingChanged.bind(this);
-    this.onShowDragIconsChanged = this.onShowDragIconsChanged.bind(this);
-
-    this.state = {
-      issues:[],
-      //issues: issueList,
-      allowDropInsideItem: true,
-      allowReordering: true,
-      showDragIcons: false,
-      mode: 'select',
-      allowSearch: true,
-    };
-  }
-
-  async componentDidMount()
-  {
-    let a=0;
-    let issues1 = [];
-    let x = await fetchIssueList();
-    console.log("1 inside componentDidMount: ",x);
-    let y = x.issues.map((element) => {
-      console.log("1.5 inside componentDidMount: ",element);
-      debugger;
-          let item = {
-                ID: element.id,
-                Head_ID: (element.fields.issuelinks.length != 0
-                ? element.fields.issuelinks[0].hasOwnProperty("outwardIssue")
-                ? element.fields.issuelinks[0].outwardIssue.id : -1 : -1),
-                Issue_Key: element.key,
-                Issue_Type: element.fields.issuetype.name,
-                Summary: element.fields.summary,
-                Assignee: (element.fields.assignee === null? "Unassigned":element.fields.assignee.displayName),
-                Reporter: element.fields.reporter.displayName,
-                Priority: element.fields.priority.name,
-            }
-          console.log("2 inside componentDidMount: ",JSON.stringify(item));
-        console.log("3 inside componentDidMount: ",JSON.stringify(issues1));
-        return item;
-      });
-      console.log("4 inside componentDidMount new test: ",y);
-        this.setState({
-          issues:y,
-        });
-  }
-
-  render() {
-    const { mode, allowSearch } = this.state;
-    
-    return (
-      <div>
-        <TreeList
-          id="issues"
-          dataSource={this.state.issues}
-          rootValue={-1}
-          showRowLines={true}
-          showBorders={true}
-          defaultExpandedRowKeys={expandedRowKeys}
-          columnAutoWidth={true}
-          keyExpr="ID"
-          parentIdExpr="Head_ID"
-          onInitNewRow={this.onInitNewRow}
-          onRowInserted={this.addRow}
-        >
-
-        <RowDragging
-            onDragChange={this.onDragChange}
-            onReorder={this.onReorder}
-            allowDropInsideItem={this.state.allowDropInsideItem}
-            allowReordering={this.state.allowReordering}
-            showDragIcons={this.state.showDragIcons}
-        />
-
-        <Editing
-            allowUpdating={true}
-            allowDeleting={true}
-            allowAdding={true}
-            mode="row" 
-        />
-
-        <Column allowHiding={false} dataField="Issue_Key"> </Column>
-        <Column allowHiding={false} dataField="Issue_Type"> <RequiredRule /> <Lookup dataSource={issuestype} /> </Column>
-        <Column allowHiding={false} dataField="Summary"> <RequiredRule /> </Column>
-        <Column dataField="Assignee"> <RequiredRule /> </Column>
-        <Column dataField="Priority"> <RequiredRule /> </Column>
-        <Column allowHiding={false} type="buttons" caption="Actions">
-                <Button name="add" />
-                <Button name="edit" />
-                <Button name="delete" />
-                <Button name="save" onClick={this.addNode} />
-            </Column>
-        <ColumnChooser enabled={true} allowSearch={allowSearch} mode={mode} />
-        </TreeList>
-      </div>
-    );
-  }
-
-  addNode = async (e) => {
-    addRow(e);
-  }
+function App() {
+  const [currentIssues, setCurrentIssues] = useState(issues);
   
-  addRow = async (e) => {
-    console.log("1 inside addRow: ",e.data.Summary);
-    console.log("1.5 inside addRow: ",e.data.Issue_Type);
-    let body = {
-      fields: {
-        summary: e.data.Summary,
-        project: {
-          key: "OEM",
-        },
-        issuetype: {
-          name: e.data.Issue_Type,
-        },
-        assignee: {
-          name: "Abhishek Srivastava",
-        },
-        "customfield_10042": "https://google.com",
-        "customfield_10034": 8
-      }
-    };
-
-    let body1 = JSON.stringify(body);
-    console.log("2 inside addRow: ",JSON.stringify(body));
-    const response = await requestJira('/rest/api/3/issue', {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-      },
-      body: body1
-    })
-  console.log(`Response: ${response.status} ${response.statusText}`);
-  }
-
-  onInitNewRow(e) {
-    console.log("1 onInitNewRow: ",e);
-    e.data.ID=i;
-    e.data.Head_ID = -1;
-    i++;
-  }
-
-  onDragChange(e) {
-    console.log("1 onDragChange: ",e);
-    const visibleRows = e.component.getVisibleRows();
-    const sourceNode = e.component.getNodeByKey(e.itemData.ID);
-    let targetNode = visibleRows[e.toIndex].node;
+  const onDragChange = (e) => {
+    let visibleRows = e.component.getVisibleRows(),
+      sourceNode = e.component.getNodeByKey(e.itemData.ID),
+      targetNode = visibleRows[e.toIndex].node;
 
     while (targetNode && targetNode.data) {
       if (targetNode.data.ID === sourceNode.data.ID) {
@@ -183,62 +269,82 @@ class App extends React.Component {
     }
   }
 
-  onReorder(e) {
-    console.log("1 onReorder: ",e);
-    const visibleRows = e.component.getVisibleRows();
-    let sourceData = e.itemData;
-    let { issues } = this.state;
-    const sourceIndex = issues.indexOf(sourceData);
+  const onReorder = (e) => {
+    let visibleRows = e.component.getVisibleRows(),
+      sourceData = e.itemData,
+      targetData = visibleRows[e.toIndex].data,
+      issuesReordered = currentIssues,
+      sourceIndex = issuesReordered.indexOf(sourceData),
+      targetIndex = issuesReordered.indexOf(targetData);
 
     if (e.dropInsideItem) {
-      sourceData = { ...sourceData, Head_ID: visibleRows[e.toIndex].key };
-      issues = [
-        ...issues.slice(0, sourceIndex),
-        sourceData,
-        ...issues.slice(sourceIndex + 1),
-      ];
+      sourceData = { ...sourceData, HeadID: targetData.ID };
+      issuesReordered = [...issuesReordered.slice(0, sourceIndex), sourceData, ...issuesReordered.slice(sourceIndex + 1)];
     } else {
-      const toIndex = e.fromIndex > e.toIndex ? e.toIndex - 1 : e.toIndex;
-      let targetData = toIndex >= 0 ? visibleRows[toIndex].node.data : null;
-
-      if (targetData && e.component.isRowExpanded(targetData.ID)) {
-        sourceData = { ...sourceData, Head_ID: targetData.ID };
-        targetData = null;
-      } else {
-        const headId = targetData ? targetData.Head_ID : -1;
-        if (sourceData.Head_ID !== headId) {
-          sourceData = { ...sourceData, Head_ID: headId };
+      if (sourceData.HeadID !== targetData.HeadID) {
+        sourceData = { ...sourceData, HeadID: targetData.HeadID };
+        if (e.toIndex > e.fromIndex) {
+          targetIndex++;
         }
       }
-
-      issues = [...issues.slice(0, sourceIndex), ...issues.slice(sourceIndex + 1)];
-
-      const targetIndex = issues.indexOf(targetData) + 1;
-      issues = [...issues.slice(0, targetIndex), sourceData, ...issues.slice(targetIndex)];
+      issuesReordered = [...issuesReordered.slice(0, sourceIndex), ...issuesReordered.slice(sourceIndex + 1)];
+      issuesReordered = [...issuesReordered.slice(0, targetIndex), sourceData, ...issuesReordered.slice(targetIndex)];
     }
 
-    this.setState({
-      issues,
-    });
+    setCurrentIssues(issuesReordered);
   }
 
-  onAllowDropInsideItemChanged(args) {
-    this.setState({
-      allowDropInsideItem: args.value,
-    });
-  }
+  return (
+    <div className="App">
+      <TreeList
+        id="issueList"
+        dataSource={currentIssues}
+        rootValue={-1}
+        keyExpr="ID"
+        parentIdExpr="HeadID"
+        autoExpandAll={false} //changed from OG
+        showRowLines={true}
+        showBorders={true}
+        allowColumnReordering={true}
+        allowColumnResizing={true}
+        columnAutoWidth={true}>
 
-  onAllowReorderingChanged(args) {
-    this.setState({
-      allowReordering: args.value,
-    });
-  }
+        <Column allowHiding={false} dataField="Issue_Key"> </Column>
+        <Column allowHiding={false} dataField="Issue_Type"> <RequiredRule /> <Lookup dataSource={issuestype} /> </Column>
+        <Column allowHiding={false} dataField="Summary"> <RequiredRule /> </Column>
+        <Column dataField="Assignee"> <RequiredRule /> </Column>
+        <Column dataField="Priority"> <RequiredRule /> </Column>
 
-  onShowDragIconsChanged(args) {
-    this.setState({
-      showDragIcons: args.value,
-    });
-  }
+        {/*<ColumnFixing enabled={true} />*/}
+
+        <ColumnChooser enabled={true} />
+        <FilterRow visible={true} />
+        <SearchPanel visible={true} />
+        <Editing
+          mode="popup"
+          allowUpdating={true}
+          allowDeleting={true}
+          allowAdding={true}
+        />
+        {/*<Selection mode="single" />*/}
+
+        <RowDragging
+          onDragChange={onDragChange}
+          onReorder={onReorder}
+          allowDropInsideItem={true}
+          allowReordering={true}
+          showDragIcons={false}
+        />
+
+        <Paging
+          enabled={true}
+          defaultPageSize={10} 
+        />
+
+      </TreeList>
+      
+    </div>
+  );
 }
 
 export default App;
--- JiraTreeView/static/hello-world/src/data.js
+++ JiraTreeView/static/hello-world/src/data.js
@@ -3,9 +3,9 @@ import ForgeUI, { useProductContext } from '@forge/ui';
 import { requestJira } from '@forge/bridge';
 
 const projectKey = `OEM`;
-// let issues1 = [];
+let issues1 = [];
 
-export const fetchIssueList = async () =>{
+const fetchIssueList = async () =>{
               const context = useProductContext();
               console.log("1 inside fetchIssueList: ",context);
               console.log("2 inside fetchIssueList: ",JSON.stringify(context));
@@ -18,50 +18,28 @@ export const fetchIssueList = async () =>{
               return data;
           }
 
-// export const issues =  fetchIssueList().then(result => 
-//             {
-//               console.log("5 inside issues.",result);
-//               result.issues.forEach((element) => {
-//                 let item = {
-//                         ID: element.id,
-//                         Head_ID: getHeadId(element),
-//                         Issue_Key: element.key,
-//                         Issue_Type: element.fields.issuetype.name,
-//                         Summary: element.fields.summary,
-//                         Assignee: (element.fields.assignee === null? "Unassigned":element.fields.assignee.displayName),
-//                         Reporter: element.fields.reporter.displayName,
-//                         Priority: element.fields.priority.name,
-//                   }
-//               issues1.push(item);
-//               //issueLongList.push(item);
-//               console.log("6 inside issues: ",JSON.stringify(item));
-//               });
-//               console.log("7 inside issues: ",JSON.stringify(issues1));
-//               return issues1;
-//           });
+export const issues =  fetchIssueList().then(result => 
+            {
+              console.log("5 inside issues.",result);
+              result.issues.forEach((element) => {
+                let item = 
+                {
+                    ID: element.id,
+                    Head_ID: (element.fields.issuelinks.length != 0
+                    ? element.fields.issuelinks[0].hasOwnProperty("outwardIssue")
+                    ? element.fields.issuelinks[0].outwardIssue.id : -1 : -1),
+                    Issue_Key: element.key,
+                    Issue_Type: element.fields.issuetype.name,
+                    Summary: element.fields.summary,
+                    Assignee: (element.fields.assignee === null? "Unassigned":element.fields.assignee.displayName),
+                    Reporter: element.fields.reporter.displayName,
+                    Priority: element.fields.priority.name,
+                }
+              issues1.push(item);
+              console.log("6 inside issues: ",JSON.stringify(item));
+              });
+              console.log("7 inside issues: ",JSON.stringify(issues1));
+              return issues1;
+          });
 
-// const getHeadId = (element) => 
-//               {
-//                     console.log("1 inside getId: ",element);
-//                             if(element.fields.issuelinks.length != 0)
-//                               {
-//                                 console.log("1.1 inside getId: ",element.fields.issuelinks.length);
-//                                 debugger;
-//                                   if (element.fields.issuelinks[0].hasOwnProperty("outwardIssue"))
-//                                     {
-//                                       console.log("1.2 inside getId inside else inside if: ");
-//                                       return element.fields.issuelinks[0].outwardIssue.id
-//                                     }
-//                                   else
-//                                     {
-//                                       console.log("1.4 inside getId inside else inside else: ");
-//                                     return -1
-//                                     }
-//                                   return -1;
-//                               }
-//                             else
-//                               {
-//                                 console.log("1.5 inside getId: ");
-//                                 return -1;
-//                               }
-//               }
+export default issues;
