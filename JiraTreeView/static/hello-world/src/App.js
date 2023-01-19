639c1f5909df44aecab766d4e15e2116c919a40d
ABHISHEK611 <abhi.scorpio611@gmail.com>
2023-01-19 12:31:16 +0530

    changes local v1.0

--- JiraTreeView/static/hello-world/src/App.js
+++ JiraTreeView/static/hello-world/src/App.js
@@ -159,27 +159,33 @@ function App() {
       sourceIndex = issuesReordered.indexOf(sourceData),
       targetIndex = issuesReordered.indexOf(targetData);
 
-    console.log("1 inside onReorder: ",visibleRows);
-    console.log("2 inside onReorder: ",sourceData);
-    console.log("3 inside onReorder: ",targetData);
-    console.log("4 inside onReorder: ",issuesReordered);
-    console.log("5 inside onReorder: ",sourceIndex);
-    console.log("6 inside onReorder: ",targetIndex);
+    console.log("1 inside onReorder visible rows: ",visibleRows);
+    console.log("2 inside onReorder source: ",sourceData);
+    console.log("3 inside onReorder target: ",targetData);
+    //console.log("4 inside onReorder: ",issuesReordered);
+    console.log("5 inside onReorder sourceIndex: ",sourceIndex);
+    console.log("6 inside onReorder targetIndex: ",targetIndex);
     if (e.dropInsideItem) {
+      console.log("7 inside onReorder inside if:");
       sourceData = { ...sourceData, Head_ID: targetData.ID };
       issuesReordered = [...issuesReordered.slice(0, sourceIndex), sourceData, ...issuesReordered.slice(sourceIndex + 1)];
       savingDragandDrop(sourceData, targetData);
     } else {
-      if (sourceData.Head_ID !== targetData.Head_ID) {
+      console.log("8 inside onReorder inside else:");
+      if (sourceData.Head_ID !== targetData.Head_ID) 
+      {
+        console.log("9 inside onReorder inside else 1stif:");
         sourceData = { ...sourceData, Head_ID: targetData.Head_ID };
         if (e.toIndex > e.fromIndex) {
+          console.log("10 inside onReorder inside else 2ndif:");
           targetIndex++;
         }
       }
       issuesReordered = [...issuesReordered.slice(0, sourceIndex), ...issuesReordered.slice(sourceIndex + 1)];
+      console.log("11 inside onReorder inside else:",issuesReordered);
       issuesReordered = [...issuesReordered.slice(0, targetIndex), sourceData, ...issuesReordered.slice(targetIndex)];
+      console.log("12 inside onReorder inside else:",issuesReordered);
     }
-
     setCurrentIssues(issuesReordered);
   }
 
