3f307e8dbee5f6c7e3df29c34b863da3e2e1b25d
ABHISHEK611 <abhi.scorpio611@gmail.com>
2023-01-23 18:20:10 +0530

    changes hierarchy v3.0

--- JiraTreeView/static/hello-world/src/App.js
+++ JiraTreeView/static/hello-world/src/App.js
@@ -103,11 +103,7 @@ function App() {
       if(e.row.data.Head_ID != -1)
       {
         console.log("4.5 inside dataLink: ",e.row.data.Head_ID);
-        const responseLink = await api.asApp().requestJira(route`/rest/api/2/issue/${e.row.data.Head_ID}`, {
-          headers: {
-            'Accept': 'application/json'
-          }
-        });
+        const responseLink = await requestJira(`/rest/api/2/issue/${e.row.data.Head_ID}`);
         console.log("5 responseLink: ",JSON.stringify(responseLink));
         const dataLink = await responseLink.json();
         console.log("5.5 dataLink in json:",dataLink);
